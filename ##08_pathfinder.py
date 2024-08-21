084f4a4bc92af9e495bf2a190b1aec502b0a79e7
Ayusshi Singh <140041893+ayusshisingh@users.noreply.github.com>
2024-08-21 16:58:34 +0800

    Create ##08_pathfinder.py

---
+++ ##08_pathfinder.py
@@ -0,0 +1,39 @@
+import curses
+from curses import wrapper
+import time
+import queue
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                stdscr.addstr(i, j*2, "O", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
+
+def main(stdscr):
+  #add colors
+  curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+  stdscr.clear()
+  print_maze(maze, stdscr)
+  stdscr.refresh()
+  stdscr.getch()
+
+wrapper(main)
+
