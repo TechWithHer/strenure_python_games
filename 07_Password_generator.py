c6c64d4efcb9f7079b5e2680b1c89c4948ae556c
Ayusshi Singh <140041893+ayusshisingh@users.noreply.github.com>
2024-08-21 14:37:53 +0800

    Update 07_Password_generator.py

--- 07_Password_generator.py
+++ 07_Password_generator.py
@@ -1,29 +1,59 @@
 import random
 import string
 
-#define a function
+# Define a function to generate a password
+def generate_password(length, special_char, numbers):
+    print("Let me suggest a password for you")
 
-def generate_password(length, special_char=True, numbers=True):
-  print("Let me suggest a password for you")
+    # Character set for password  
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation.replace(" ", "")
 
-#character set for password  
-  letters = string.ascii_letters
-  digits = string.digits
-  special = string.punctuation
- 
-  character = letters
-  password = ""
-  
-  if special_char:
-   character += special
-  if numbers:
-   character += digits
-  else:
-   quit()
-    
-  while len(password) < length:
-    password += random.choice(character)
-  
-  print(password)
+    # Actual calculation for password 
+    character = letters
+    password = ""
+    if special_char and numbers:
+        character += special
+        character += digits
+    elif special_char == 'False' and numbers == 'True':
+        character += digits
+    elif special_char == 'True' and numbers == 'False':
+        character += special
+    else:
+        print("Something incorrect happened")
+        quit()
 
-generate_password(8)
+    while len(password) < length:
+        password += random.choice(character)
+
+    print(password)
+
+# Setting up the default value for the parameters
+sc = 'True'
+num = 'True'
+
+print("Let's generate a password for you")
+pass_length = input("Enter the password length: ")
+
+# For special_char
+special_char = input("Do you want special characters in your password? Y/N ").lower()
+if special_char == "y":
+    sc = 'True'
+elif special_char == "n":
+    sc = 'False'
+else:
+    print("Incorrect value input")
+    quit()
+
+# Ask user for numbers
+numbers = input("Do you want numbers in your password? Y/N ").lower()
+if numbers == "y":
+    num = 'True'
+elif numbers == "n":
+    num = 'False'
+else:
+    print("Incorrect value input")
+    quit()
+
+generate_password(int(pass_length), sc, num)
